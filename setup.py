c32fa9250d49bbe37c8dca1dbaa5db28710250bf
DW <dwhan89@gmail.com>
2020-01-20 17:33:39 -0500

    initial updates

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import os
+import subprocess as sp
+import sys
+from distutils.errors import DistutilsError
+from distutils.sysconfig import get_config_var, get_config_vars
+
+import numpy as np
+import setuptools
+import versioneer
+from numpy.distutils.core import setup, Extension, build_ext, build_src
+
+build_ext = build_ext.build_ext
+build_src = build_src.build_src
+
+setup(
+    author="Dongwon 'DW' Han",
+    author_email='dwhan89@gmail.com',
+    classifiers=[
+        'Development Status :: Pre-Alpha',
+        'Intended Audience :: Mostly Me',
+        'License :: Apache LICENSE-2.0',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="gan",
+    package_dir={"crush": "gan"},
+    install_requires=["torch",
+                      "numpy >= 1.10",
+                      "pixell",
+                      "scipy",
+                      "mpi4py",
+                      "pandas"],
+    license="Apache LICENSE-2.0",
+    keywords='gan',
+    name='GAN',
+    packages=['gan'],
+    include_package_data=True,
+    package_data={'gan': ['data/*']},
+    url='https://github.com/dwhan89/gan',
+)
+
+print('\n[setup.py request was successful.]')

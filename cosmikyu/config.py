8c00f3b03bfe8199812d22c36b6e3717758043be
dw <dwhan89@gmail.com>
2020-02-16 20:42:14 -0500

    wgan wrapper

--- cosmikyu/config.py
+++ cosmikyu/config.py
@@ -6,6 +6,7 @@ _root = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 # default directory list
 default_log_dir = os.path.join(_root, "log")
 default_data_dir = os.path.join(_root, "data")
+default_output_dir = os.path.join(_root, "output")
 
 
 argparser = argparse.ArgumentParser(
--- cosmikyu/gan.py
+++ cosmikyu/gan.py
@@ -1,23 +1,121 @@
+from cosmikyu import config
 import numpy as np
+import os
 
-import torchvision.transforms as transforms
 from torchvision.utils import save_image
-
-from torch.utils.data import DataLoader
-from torchvision import datasets
 from torch.autograd import Variable
 
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
 
 
+class WGAN(object):
+    def __init__(self, identifier, shape, latent_dim, output_path=None, sample_path=None, cuda=False, ngpu=1):
+        self.cuda = cuda
+        if not self.cuda:
+            ngpu = 0
+        self.shape = shape
+        self.latent_dim = latent_dim
+        self.identifier = identifier
+
+        if output_path is None:
+            output_path = os.path.join(config.default_output_dir, identifier)
+        if sample_path is None:
+            sample_path = os.path.join(output_path, "sample")
+        self.output_path = output_path
+        self.sample_path = sample_path
+
+        os.makedirs(self.output_path)
+        os.makedirs(self.sample_path)
+
+        if torch.cuda.is_available() and not self.cuda:
+            print("[WARNING] You have a CUDA device. You probably want to run with CUDA enabled")
+        self.device = torch.device("cuda:0" if self.cuda else "cpu")
+
+        self.generator = WGAN_Generator(shape, latent_dim, ngpu=ngpu).to(device=self.device)
+        self.discriminator = WGAN_Discriminator(shape, ngpu=ngpu).to(device=self.device)
+
+
+    def load_states(self):
+        generator_state_file = os.path.join(self.output_path, "generator.pt")
+        discriminator_state_file = os.path.join(self.output_path, "discriminator.pt")
+        try:
+            print("loading saved states")
+            self.generator.load_state_dict(torch.load(generator_state_file))
+            self.discriminator.load_state_dict(torch.load(discriminator_state_file))
+        except Exception:
+            print("failed to load saved states")
+
+    def save_states(self):
+        print("saving states")
+        generator_state_file = os.path.join(self.output_path, "generator.pt")
+        discriminator_state_file = os.path.join(self.output_path, "discriminator.pt")
+        torch.save(self.generator.state_dict(), generator_state_file)
+        torch.save(self.discriminator.state_dict(), discriminator_state_file)
+
+    def train(self, dataloader, lr=0.00005, nepochs=200, clip_tresh=0.01, num_critic=5, sample_interval=1000,
+              save_interval=50000, load_states=True, save_states=True, verbose=True):
+
+        if load_states:
+            self.load_states()
+
+        opt_gen = torch.optim.RMSprop(self.generator.parameters(), lr=lr)
+        opt_disc = torch.optim.RMSprop(self.discriminator.parameters(), lr=lr)
+        Tensor = torch.cuda.FloatTensor if self.cuda else torch.FloatTensor
+
+        batches_done = 0
+        for epoch in range(nepochs):
+            for i, (imgs, _) in enumerate(dataloader):
+                real_imgs = Variable(imgs.type(Tensor))
+
+                opt_disc.zero_grad()
+
+                # Sample noise as generator input
+                z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], self.latent_dim))))
+
+                # Generate a batch of images
+                fake_imgs = self.generator(z).detach()
+                # Adversarial loss
+                loss_D = -torch.mean(self.discriminator(real_imgs)) + torch.mean(self.discriminator(fake_imgs))
+
+                loss_D.backward()
+                opt_disc.step()
+
+                # Clip weights of discriminator
+                for p in self.discriminator.parameters():
+                    p.data.clamp_(-clip_tresh, clip_tresh)
+
+                if i % num_critic == 0:
+                    opt_gen.zero_grad()
+
+                    # Generate a batch of images
+                    gen_imgs = self.generator(z)
+                    # Adversarial loss
+                    loss_G = -torch.mean(self.discriminator(gen_imgs))
+
+                    loss_G.backward()
+                    opt_gen.step()
+
+                    if verbose:
+                        print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                            % (epoch, nepochs, batches_done % len(dataloader), len(dataloader), loss_D.item(), loss_G.item())
+                        )
+
+                if batches_done % sample_interval == 0:
+                    save_image(gen_imgs.data[:5], os.path.join(self.sample_path, "%d.png" % batches_done), normalize=True)
+                if batches_done % save_interval == 0 and save_states:
+                    self.save_states()
+                batches_done += 1
+
+        if save_states:
+            self.save_states()
 
 class WGAN_Generator(nn.Module):
-    def __init__(self, shape, latent_dim):
+    def __init__(self, shape, latent_dim, ngpu=1):
         super(WGAN_Generator, self).__init__()
         self.shape = shape
         self.latent_dim = latent_dim
+        self.ngpu = ngpu
 
         def custom_layer(dim_in, dim_out, batch_normalize=True):
             layers = [nn.Linear(dim_in, dim_out)]
@@ -36,15 +134,20 @@ class WGAN_Generator(nn.Module):
         )
 
     def forward(self, z):
-        img = self.model(z)
+        if z.is_cuda and self.ngpu > 1:
+            img = nn.parallel.data_parallel(self.model, z, range(self.ngpu))
+        else:
+            img = self.model(z)
         img = img.view(img.shape[0], *self.shape)
         return img
 
+
 class WGAN_Discriminator(nn.Module):
-    def __init__(self, shape):
+    def __init__(self, shape, ngpu=1):
         super(WGAN_Discriminator, self).__init__()
         self.shape = shape
-        
+        self.ngpu = ngpu
+
         self.model = nn.Sequential(
             nn.Linear(int(np.prod(self.shape)), 512),
             nn.LeakyReLU(0.2, inplace=True),
@@ -55,5 +158,8 @@ class WGAN_Discriminator(nn.Module):
 
     def forward(self, img):
         flattened = img.view(img.shape[0], -1)
-        ret = self.model(flattened)
+        if img.is_cuda and self.ngpu > 1:
+            ret = nn.parallel.data_parallel(self.model, flattened, range(self.ngpu))
+        else:
+            ret = self.model(flattened)
         return ret

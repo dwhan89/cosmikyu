2942bc62dc58299c8b5e42a63d33c09bb1dd7ecc
DW <retimes@hotmail.com>
2020-02-26 12:42:20 -0800

    updates

--- cosmikyu/maps.py
+++ cosmikyu/maps.py
@@ -9,19 +9,26 @@ H_CGS = 6.62608e-27
 K_CGS = 1.3806488e-16
 C_light = 2.99792e+10
 
-def get_pos(dec, ra, patch_width, res, shape=None):
-    '''
-    :param dec: The declination of the center of a patch in radian
-    :param ra: The right ascension of the center of a patch in radian
-    :param patch_width: The height and the width of a patch in radian
-    :param res: The resolution of a patch
-    :param shape: optional (Ny, Nx)
-    :return: (shape, wcs)
-    '''
-    y0, x0 = (dec - patch_width / 2, ra - patch_width / 2)
-    y1, x1 = (y0 + patch_width, x0 + patch_width)
-    return enmap.geometry([[y0,x0],[y1,x1]], res=res, shape=shape)
+def rand_geometry(width, height, res=None, shape=None, seed=None):
+    np.random.seed(seed)
+    ypos = np.random.uniform(-1*np.pi/2., np.pi/2.)
+    xpos = np.random.uniform(-1*np.pi, np.pi)
+    return get_geometry(xpos, ypos, width, height, res=res, shape=shape)
+    
 
+def get_geometry(xpos, ypos, width, height, res=None, shape=None):
+    if shape is None:
+        nx = int(np.ceil(width/res))
+        ny = int(np.ceil(height/res))
+        shape = (ny, nx)
+    else:pass
+    pos = [[-1.*height/2.+ypos, -1.*width/2.+xpos], [height/2.+ypos, width/2.+xpos]]
+    shape, wcs = enmap.geometry(pos=pos, shape=shape)
+    return (shape, wcs, pos)
+    
+
+def get_template(shape, wcs):
+    return enmap.zeros(shape, wcs)
 
 def fnu(nu,tcmb=default_tcmb):
     """

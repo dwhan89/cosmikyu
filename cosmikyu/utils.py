145cf0c3b5ad4c507022827c1e65304a6d5fc6ec
DW <dwhan89@gmail.com>
2020-02-26 15:50:25 -0500

    updates

--- cosmikyu/utils.py
+++ cosmikyu/utils.py
@@ -1,13 +1,9 @@
-import os
 
-
-def create_dir(path_to_dir):
-    """
-    check whether the directory already exists. if not, create it
-    """
-
-    try:
-        os.makedirs(path_to_dir)
-        print("created {}".format(path_to_dir))
-    except Exception as err:
-        print(err)
+        
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise TypeError("Can't convert 'str' object to 'boolean'")

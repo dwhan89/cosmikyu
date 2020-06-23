d7c2f1028e47cad89518564ae28c0f7e4da0a7e2
DW <retimes@hotmail.com>
2020-06-23 18:38:16 -0400

    refactored gan.py

--- cosmikyu/gan.py
+++ cosmikyu/gan.py
@@ -31,9 +31,9 @@ class GAN(object):
         self.generator = None
         self.discriminator = None
 
-    def load_states(self):
-        generator_state_file = os.path.join(self.output_path, "generator.pt")
-        discriminator_state_file = os.path.join(self.output_path, "discriminator.pt")
+    def load_states(self, output_path):
+        generator_state_file = os.path.join(output_path, "generator.pt")
+        discriminator_state_file = os.path.join(output_path, "discriminator.pt")
 
         try:
             print("loading saved states")
@@ -42,32 +42,47 @@ class GAN(object):
         except Exception:
             print("failed to load saved states")
 
-    def save_states(self):
+    def save_states(self, output_path):
         print("saving states")
-        generator_state_file = os.path.join(self.output_path, "generator.pt")
-        discriminator_state_file = os.path.join(self.output_path, "discriminator.pt")
+        generator_state_file = os.path.join(output_path, "generator.pt")
+        discriminator_state_file = os.path.join(output_path, "discriminator.pt")
         torch.save(self.generator.state_dict(), generator_state_file)
         torch.save(self.discriminator.state_dict(), discriminator_state_file)
 
-    def generate_image(self, num_imgs, seed=None):
-        if not seed: np.random.seed(seed)
-        Tensor = torch.cuda.FloatTensor if self.cuda else torch.FloatTensor
-        z = Variable(Tensor(np.random.normal(0, 1, (num_imgs, self.latent_dim))))
-        return self.generator(z).detach()
+    def _get_optimizers(self, **kwargs):
+        raise NotImplemented()        
 
-    def train(self, dataloader, lr=0.00005, nepochs=200, clip_tresh=0.01, num_critic=5, sample_interval=1000,
-              save_interval=10000, load_states=True, save_states=True, verbose=True, visdom_plotter=None, runid="trial"):
+    def _post_process_discriminator(self, **kwargs):
+        pass
 
-        run_path = os.path.join(self.experiment_path, runid)
+    def _eval_discriminator_loss(self, real_imgs, gen_imgs): 
+        raise NotImplemented()
+    
+    def _eval_generator_loss(self, real_imgs, gen_imgs): 
+        raise NotImplemented()
+    
+    def train(self, dataloader, nepochs=200, ncritics=5, sample_interval=1000,
+              save_interval=10000, load_states=True, save_states=True, verbose=True, visdom_plotter=None, mlflow_run=None, **kwargs):
+        kwargs.update({"nepochs": nepochs, "ncritics": ncritics})
+        # Logging parameters
+        if mlflow_run:
+            for key, value in kwargs.items():
+                mlflow.log_param(key, value)
+
+        # Base Setup
+        run_id = "trial" if not mlflow_run else mlflow_run.info.run_id
+        run_path = os.path.join(self.experiment_path, run_id)
+        
+        os.makedirs(run_path, exist_ok=True) 
         if load_states:
-            self.load_states()
-
-        opt_gen = torch.optim.RMSprop(self.generator.parameters(), lr=lr)
-        opt_disc = torch.optim.RMSprop(self.discriminator.parameters(), lr=lr)
+            self.load_states(run_path)
+        
         Tensor = torch.cuda.FloatTensor if self.cuda else torch.FloatTensor
+
+        # Get Optimizers
+        opt_gen, opt_disc = self._get_optimizers(**kwargs)
 
         batches_done = 0
-
         for epoch in range(nepochs):
             for i, sample in enumerate(dataloader):
                 imgs = sample[0]
@@ -79,24 +94,22 @@ class GAN(object):
                 z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], self.latent_dim))))
 
                 # Generate a batch of images
-                fake_imgs = self.generator(z).detach()
+                gen_imgs = self.generator(z).detach()
                 # Adversarial loss
-                loss_D = -torch.mean(self.discriminator(real_imgs)) + torch.mean(self.discriminator(fake_imgs))
-
+                loss_D = self._eval_discriminator_loss(real_imgs, gen_imgs)
                 loss_D.backward()
                 opt_disc.step()
 
-                # Clip weights of discriminator
-                for p in self.discriminator.parameters():
-                    p.data.clamp_(-clip_tresh, clip_tresh)
+                # Hook for Discriminator Post Processing
+                self._post_process_discriminator(**kwargs)
 
-                if i % num_critic == 0:
+                if i % ncritics == 0:
                     opt_gen.zero_grad()
 
                     # Generate a batch of images
                     gen_imgs = self.generator(z)
-                    # Adversarial loss
-                    loss_G = -torch.mean(self.discriminator(gen_imgs))
+                    # Adversarial loss\
+                    loss_G = self._eval_generator_loss(real_imgs, gen_imgs)
 
                     loss_G.backward()
                     opt_gen.step()
@@ -110,7 +123,7 @@ class GAN(object):
                 if batches_done % sample_interval == 0:
                     save_image(gen_imgs.data[:5], os.path.join(run_path, "%d.png" % batches_done), normalize=True)
                 if batches_done % save_interval == 0 and save_states:
-                    self.save_states()
+                    self.save_states(run_path)
                 batches_done += 1
 
                 if visdom_plotter is not None:
@@ -118,10 +131,12 @@ class GAN(object):
                                         xlabel='batches_done')
                     visdom_plotter.plot("G loss", 'G_loss', np.array([batches_done]), np.array([-1 * loss_G.item()]),
                                         xlabel='batches_done')
+        #self._end_train(save_states)
         if save_states:
-            self.save_states()
+            self.save_states(run_path)
+    
 
-
+        
 class WGAN(GAN):
     def __init__(self, identifier, shape, latent_dim, output_path=None, experiment_path=None, cuda=False, ngpu=1):
         super().__init__(identifier, shape, latent_dim, output_path=output_path, experiment_path=experiment_path, cuda=cuda, ngpu=ngpu)
@@ -129,6 +144,29 @@ class WGAN(GAN):
         self.generator = WGAN_Generator(shape, latent_dim, ngpu=self.ngpu).to(device=self.device)
         self.discriminator = WGAN_Discriminator(shape, ngpu=self.ngpu).to(device=self.device)
 
+    def _post_process_discriminator(self, **kwargs):
+        clip_tresh = kwargs["clip_tresh"]
+        # Clip weights of discriminator
+        for p in self.discriminator.parameters():
+            p.data.clamp_(-clip_tresh, clip_tresh)     
+
+    def _eval_discriminator_loss(self, real_imgs, gen_imgs):         
+        return -torch.mean(self.discriminator(real_imgs)) + torch.mean(self.discriminator(gen_imgs))
+
+    def _get_optimizers(self, **kwargs):
+        opt_gen = torch.optim.RMSprop(self.generator.parameters(), lr=kwargs['lr'])
+        opt_disc = torch.optim.RMSprop(self.discriminator.parameters(), lr=kwargs['lr'])
+        return opt_gen, opt_disc
+
+    def _eval_generator_loss(self, real_imgs, gen_imgs): 
+        return  -torch.mean(self.discriminator(gen_imgs))
+    
+    def train(self, dataloader, nepochs=200, ncritics=5, sample_interval=1000,
+              save_interval=10000, load_states=True, save_states=True, verbose=True, visdom_plotter=None, mlflow_run=None, lr=0.00005, clip_tresh=0.01):
+
+        super().train(dataloader, nepochs=nepochs, ncritics=ncritics, sample_interval=sample_interval,
+              save_interval=save_interval, load_states=load_states, save_states=save_states, verbose=verbose, visdom_plotter=visdom_plotter, mlflow_run=mlflow_run,
+              lr=lr, clip_tresh=clip_tresh)
 
 class WGAN_Generator(nn.Module):
     def __init__(self, shape, latent_dim, ngpu=1):

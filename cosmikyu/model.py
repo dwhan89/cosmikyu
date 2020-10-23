1c2911aa7b7b00b71a38291da178973efab02288
dw <dwhan89@gmail.com>
2020-10-23 12:16:38 -0400

    unet patchgan implementation

--- cosmikyu/model.py
+++ cosmikyu/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from torch import nn as nn
+import torch
 
 import cosmikyu.nn as cnn
 
@@ -61,7 +62,7 @@ class WGAN_Discriminator(nn.Module):
 
 class DCGAN_SIMPLE_Generator(nn.Module):
     def __init__(self, shape, latent_dim, nconv_layer=2, nconv_fc=32, ngpu=1):
-        super(DCGAN_SIMPLE_Generator, self).__init__()
+        super().__init__()
 
         self.shape = shape
         self.nconv_layer = nconv_layer
@@ -103,7 +104,7 @@ class DCGAN_SIMPLE_Generator(nn.Module):
 
 class DCGAN_SIMPLE_Discriminator(nn.Module):
     def __init__(self, shape, nconv_layer=2, nconv_fc=32, ngpu=1):
-        super(DCGAN_SIMPLE_Discriminator, self).__init__()
+        super().__init__()
         self.shape = shape
         self.ngpu = ngpu
         self.nconv_layer = nconv_layer
@@ -196,22 +197,23 @@ class DCGAN_Discriminator_BASE(nn.Module):
 
         nconv_lc = nconv_fc * self.stride ** (self.nconv_layer - 1)
 
-        def discriminator_block(in_filters, out_filters, normalize=True):
-            block = [nn.Conv2d(in_filters, out_filters, self.kernal_size, stride=self.stride, padding=self.padding)]
-            if normalize:
-                block.append(nn.BatchNorm2d(out_filters))
-            block.append(nn.LeakyReLU(0.2, inplace=True))
-            return block
 
-        layers = [*discriminator_block(self.shape[0], nconv_fc, normalize=False)]
+        layers = [*self.discriminator_block(self.shape[0], nconv_fc, normalize=False)]
         for i in range(self.nconv_layer - 1):
             layers.extend(
-                discriminator_block(self.nconv_fc * self.stride ** i, self.nconv_fc * self.stride ** (i + 1),
+                self.discriminator_block(self.nconv_fc * self.stride ** i, self.nconv_fc * self.stride ** (i + 1),
                                     normalize=normalize))
 
         layers.extend(self.get_last_layer(nconv_lc))
 
         self.model = nn.Sequential(*layers)
+
+    def discriminator_block(self, in_filters, out_filters, normalize=True):
+        block = [nn.Conv2d(in_filters, out_filters, self.kernal_size, stride=self.stride, padding=self.padding)]
+        if normalize:
+            block.append(nn.BatchNorm2d(out_filters))
+        block.append(nn.LeakyReLU(0.2, inplace=True))
+        return block
 
     def get_last_layer(self, nconv_lc):
         raise NotImplemented()
@@ -238,4 +240,121 @@ class UNET_Discriminator(DCGAN_Discriminator_BASE):
 
     def get_last_layer(self, nconv_lc):
         nin_filt = self.nconv_fc * self.stride ** ((self.nconv_layer - 2) + 1)
-        return [nn.Conv2d(nin_filt, 1, self.kernal_size, stride=self.stride, padding=self.padding), nn.Sigmoid()]
+        layers = [*self.discriminator_block(nin_filt, nin_filt, normalize=True)]
+        layers.extend(self.discriminator_block(nin_filt, nin_filt, normalize=True))
+        layers.extend([nn.Conv2d(nin_filt, 1, self.kernal_size, stride=self.stride, padding=self.padding), nn.Sigmoid()])
+        return layers
+
+class UNET_Generator(nn.Module):
+    def __init__(self, shape, nconv_layer=2, nconv_fc=32, ngpu=1, kernal_size=5, stride=2, padding=2,
+                 output_padding=1, normalize=True, activation=None):
+        super().__init__()
+        self.shape = shape
+        self.nconv_layer = nconv_layer
+        self.normalize = normalize
+        self.ngpu = ngpu
+        self.nconv_fc = nconv_fc
+        self.kernal_size = kernal_size
+        self.stride = stride
+        self.padding = padding
+        self.output_padding = output_padding
+        self.ds_size = shape[-1] // self.stride ** self.nconv_layer
+        self.activation = activation
+        self.model_dict = nn.ModuleDict()
+        nconv_lc = nconv_fc * self.stride ** (self.nconv_layer - 1)
+        class UNetDown(nn.Module):
+            def __init__(self, in_filters, out_filters, normalize, dropout_rate=0, kernal_size=self.kernal_size,
+                         stride=self.stride, padding=self.padding):
+                super().__init__()
+                block = [nn.Conv2d(in_filters, out_filters, kernal_size, stride=stride, padding=padding)]
+                if dropout_rate != 0:
+                    assert(1 >= dropout_rate >= 0)
+                    block.append(nn.Dropout(dropout_rate))
+                if normalize:
+                    block.append(nn.BatchNorm2d(out_filters))
+                block.append(nn.LeakyReLU(0.2, inplace=True))
+                self.model = nn.Sequential(*block)
+
+            def forward(self, z):
+                if z.is_cuda and self.ngpu > 1:
+                    ret = nn.parallel.data_parallel(self.model, z, range(self.ngpu))
+                else:
+                    ret = self.model(z)
+                return ret
+
+        class UNetUP(nn.Module):
+            def __init__(self, in_filters, out_filters, normalize, dropout_rate=0, kernal_size=self.kernal_size,
+                         stride=self.stride, padding=self.padding, output_padding=self.output_padding):
+                super().__init__()
+                block = [nn.ConvTranspose2d(in_filters, out_filters, kernal_size, stride=stride, padding=padding
+                                            ,output_padding=output_padding)]
+                if normalize:
+                    block.append(nn.BatchNorm2d(out_filters))
+                if dropout_rate != 0:
+                    assert(1 >= dropout_rate >= 0)
+                    block.append(nn.Dropout(dropout_rate))
+                block.append(nn.ReLU(inplace=True))
+                self.model = nn.Sequential(*block)
+
+            def forward(self, z, skip_input):
+                if z.is_cuda and self.ngpu > 1:
+                    ret = nn.parallel.data_parallel(self.model, z, range(self.ngpu))
+                else:
+                    ret = self.model(z)
+                if skip_input is not None:
+                    ret = torch.cat((ret, skip_input),1)
+
+                return ret
+
+        self.model_dict["down0"] = UNetDown(self.shape[0], nconv_fc, normalize=False, dropout_rate=0.0)
+        for i in range(self.nconv_layer-1):
+            self.model_dict["down%d"%(i+1)] = UNetDown(self.nconv_fc * self.stride ** i,
+                                                       self.nconv_fc * self.stride ** (i + 1),
+                                                       normalize=True)
+        ## make sure i index is correct here
+        self.model_dict["down%d"%(i+2)] = UNetDown(nconv_lc, nconv_lc, normalize=True)
+        self.model_dict["down%d"%(i+3)] = UNetDown(nconv_lc, nconv_lc, normalize=True)
+        self.model_dict["down%d"%(i+4)] = UNetDown(nconv_lc, nconv_lc, normalize=True)
+        self.model_dict["up%d"%0] = UNetUP(nconv_lc, nconv_lc, normalize=True,dropout_rate=0.5)
+        self.model_dict["up%d"%1] = UNetUP(nconv_lc*2, nconv_lc, normalize=True,dropout_rate=0.5)
+        self.model_dict["up%d"%2] = UNetUP(nconv_lc*2, nconv_lc, normalize=True,dropout_rate=0.5)
+        self.model_dict["up%d"%3] = UNetUP(nconv_lc*2, int(nconv_lc/2), normalize=True, dropout_rate=0)
+        for i in range(2):
+            self.model_dict["up%d"%(i+4)] = UNetUP(nconv_lc // self.stride ** (i), nconv_lc // self.stride ** (i + 2),
+                                                    normalize=True,dropout_rate=0)
+        self.model_dict["up%d"% (i+5)] = UNetUP(nconv_lc // self.stride ** (i+1), self.shape[0],
+                                                    normalize=True,dropout_rate=0)
+        if self.activation is not None:
+            self.model_dict["final"](self.activation)
+
+    def forward(self, img):
+        ret = {"down-1": img}
+        if img.is_cuda and self.ngpu > 1:
+            for i in range(self.nconv_layer+3):
+                input_key = "down%d"%(i-1)
+                model_key = "down%d"%(i)
+                ret[model_key] = nn.parallel.data_parallel(self.model_dict[model_key], ret[input_key], range(self.ngpu))
+
+            ret.pop("down-1")
+            ret_up = ret[model_key]
+            for i in range(self.nconv_layer+3):
+                skip_key = "down%d"% (self.nconv_layer + 1-i)
+                model_key = "up%d" % (i)
+                input_wrapped = (ret_up, ret[skip_key] if skip_key in ret else None)
+                ret_up = nn.parallel.data_parallel(self.model_dict[model_key], input_wrapped, range(self.ngpu))
+
+        else:
+            for i in range(self.nconv_layer+3):
+                input_key = "down%d"%(i-1)
+                model_key = "down%d"%(i)
+                ret[model_key] = self.model_dict[model_key](ret[input_key])
+
+            ret.pop("down-1")
+            ret_up = ret[model_key]
+            for i in range(self.nconv_layer+3):
+                skip_key = "down%d"% (self.nconv_layer + 1-i)
+                model_key = "up%d" % (i)
+                ret_up = self.model_dict[model_key](ret_up, ret[skip_key] if skip_key in ret else None)
+
+        return ret_up if "final" not in self.model_dict else self.model_dict["final"](ret_up)
+

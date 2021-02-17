534e6bbfe05fdaaeab0d7ad4570066da36add3bd
DW <retimes@hotmail.com>
2021-02-17 10:35:34 -0500

    minor updates

--- cosmikyu/stats.py
+++ cosmikyu/stats.py
@@ -228,11 +228,11 @@ class FastMultBinner(object):
             for i in range(nchannels):
                 self.binners[i] = FastBINNER(ranges[0], ranges[1], nbins)
 
-    def bin(self, arr, verbose=False):
+    def bin(self, arr, weights=None, verbose=False):
         assert (arr.shape[0] == self.nchannels)
         for i in range(self.nchannels):
             if verbose: print("binning {}".format(i))
-            self.binners[i].bin(arr[i])
+            self.binners[i].bin(arr[i], weights=weights)
 
     def get_info(self):
         ret = {}
@@ -262,6 +262,6 @@ class FastBINNER(object):
 
         assert (self.bin_sizes > 0).all()
 
-    def bin(self, arr):
-        self.storage += histogram1d(arr, self.nbin, [self.minval, self.maxval])
+    def bin(self, arr, weights=None):
+        self.storage += histogram1d(arr, self.nbin, [self.minval, self.maxval], weights=weights)
         return self.bin_center, self.storage

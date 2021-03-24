b80ca228ab7e7711c470fcf1eab4fd7ecd8d5a2d
dw <dwhan89@gmail.com>
2021-03-24 10:49:53 -0400

    bin file update

--- bin/sehgal_training_dcgan.py
+++ bin/sehgal_training_dcgan.py
@@ -1,10 +1,12 @@
-from cosmikyu import gan, config, datasets, transforms, utils
-from cosmikyu import nn as cnn
 import os
-import torch
+
 import mlflow
-from orphics import maps
 import numpy as np
+import torch
+from orphics import maps
+
+from cosmikyu import gan, config, datasets, transforms
+from cosmikyu import nn as cnn
 
 data_dir = config.default_data_dir
 sehgal_dir = os.path.join(data_dir, 'sehgal')
@@ -17,7 +19,6 @@ sample_interval = 500
 save_interval = 1
 batch_size = 32
 nepochs = 100
-#norm_info_file = "/home/dwhan89/workspace/cosmikyu/data/sehgal/201020_logzshrink_normalization_info_validation.npz"
 norm_info_file = "/home/dwhan89/workspace/cosmikyu/data/sehgal/281220_logz_normalization_info_validation.npz"
 
 _, wcs = maps.rect_geometry(width_arcmin=64., px_res_arcmin=0.5)
@@ -29,11 +30,9 @@ SDN = transforms.SehgalDataNormalizerScaledLogZShrink(norm_info_file)
 SC = transforms.SehgalSubcomponets(compt_idxes)
 RF = transforms.RandomFlips(p_v=0.5, p_h=0.5)
 
-#SDS_train = datasets.SehgalDataSet(sehgal_dir, "train141020", transforms=[SDN, RF, SC], dummy_label=True, dtype=np.float64,
-#                         shape=shape)
-
-SDS_train = datasets.SehgalDataSet(sehgal_dir, "train281220_fromcat", transforms=[SDN, RF, SC], dummy_label=True, dtype=np.float64,
-                         shape=shape)
+SDS_train = datasets.SehgalDataSet(sehgal_dir, "train281220_fromcat", transforms=[SDN, RF, SC], dummy_label=True,
+                                   dtype=np.float64,
+                                   shape=shape)
 
 dataloader = torch.utils.data.DataLoader(
     SDS_train,
@@ -41,15 +40,14 @@ dataloader = torch.utils.data.DataLoader(
     shuffle=True,
 )
 
-
-LF = cnn.LinearFeature(5,5, bias=True)
-STanh = cnn.ScaledTanh(15., 2./15.)
+LF = cnn.LinearFeature(5, 5, bias=True)
+STanh = cnn.ScaledTanh(15., 2. / 15.)
 experiment_id = "63101414f2434a3e93cd28663a89f879"
 model_dir = "/home/dwhan89/workspace/cosmikyu/output/sehgal_dcganwgp_211020/{}/model".format(experiment_id)
 DCGAN_WGP = gan.DCGAN_WGP("sehgal_dcganwgp_281220", shape, latent_dim, nconv_fcgen=64,
-                          nconv_fcdis=64, cuda=cuda, ngpu=4, nconv_layer_gen=5, nconv_layer_disc=5, kernal_size=4, stride=2,
+                          nconv_fcdis=64, cuda=cuda, ngpu=4, nconv_layer_gen=5, nconv_layer_disc=5, kernal_size=4,
+                          stride=2,
                           padding=1, output_padding=0, gen_act=[LF, STanh])
-#DCGAN_WGP.load_states(model_dir, "_{}".format(0))
 mlflow.set_experiment(DCGAN_WGP.identifier)
 print(DCGAN_WGP.experiment.experiment_id)
 with mlflow.start_run(experiment_id=DCGAN_WGP.experiment.experiment_id) as mlflow_run:
@@ -69,4 +67,3 @@ with mlflow.start_run(experiment_id=DCGAN_WGP.experiment.experiment_id) as mlflo
         lambda_gp=10.,
         lambda_l1=0.
     )
-
--- bin/sehgal_training_pixgan_wgp.py
+++ bin/sehgal_training_pixgan_wgp.py
@@ -1,10 +1,12 @@
-from cosmikyu import gan, config, datasets, transforms, utils
-from cosmikyu import nn as cnn
 import os
-import torch
+
 import mlflow
-from orphics import maps
 import numpy as np
+import torch
+from orphics import maps
+
+from cosmikyu import gan, config, datasets, transforms
+from cosmikyu import nn as cnn
 
 data_dir = config.default_data_dir
 sehgal_dir = os.path.join(data_dir, 'sehgal')
@@ -16,7 +18,6 @@ sample_interval = 500
 save_interval = 1
 batch_size = 32
 nepochs = 100
-#norm_info_file = "/home/dwhan89/workspace/cosmikyu/data/sehgal/201020_logzshrink_normalization_info_validation.npz"
 norm_info_file = "/home/dwhan89/workspace/cosmikyu/data/sehgal/281220_logz_normalization_info_validation.npz"
 
 _, wcs = maps.rect_geometry(width_arcmin=64., px_res_arcmin=0.5)
@@ -27,27 +28,25 @@ os.makedirs(sehgal_dir, exist_ok=True)
 SDN = transforms.SehgalDataNormalizerScaledLogZShrink(norm_info_file)
 SC = transforms.SehgalSubcomponets(compt_idxes)
 RF = transforms.RandomFlips(p_v=0.5, p_h=0.5)
-#MCM = transforms.MultiComptMultiply([1, 0.9991, 0.9834, 0.9894, 1.0034]   )
-#SDS_train = datasets.SehgalDataSet(sehgal_dir, "train_secondary161120", transforms=[SDN, RF, SC], dummy_label=True, dtype=np.float32)
-SDS_train = datasets.SehgalDataSet(sehgal_dir, "train_secondary281220_fromcat", transforms=[SDN, RF, SC], dummy_label=True, dtype=np.float32)
+SDS_train = datasets.SehgalDataSet(sehgal_dir, "train_secondary281220_fromcat", transforms=[SDN, RF, SC],
+                                   dummy_label=True, dtype=np.float32)
 
-#SDS_train = datasets.SehgalDataSet(sehgal_dir, "train141020", transforms=[SDN, RF, SC], dummy_label=True, dtype=np.float64)
 dataloader = torch.utils.data.DataLoader(
     SDS_train,
     batch_size=batch_size,
     shuffle=True,
 )
 
-
-STanh = cnn.ScaledTanh(15, 2/15)
-LF = cnn.LinearFeature(4,4, bias=True)
+STanh = cnn.ScaledTanh(15, 2 / 15)
+LF = cnn.LinearFeature(4, 4, bias=True)
 experiment_id = "06b9a352b8bb4051b50f91660ebc4cfe"
 model_dir = "/home/dwhan89/workspace/cosmikyu/output/sehgal_pixganwgp_281220/{}/model".format(experiment_id)
 PIXGAN = gan.PIXGAN_WGP("sehgal_pixganwgp_281220", shape, nconv_fcgen=64,
-                          nconv_fcdis=64, cuda=cuda, ngpu=4, nconv_layer_gen=4, nconv_layer_disc=5, kernal_size=4, stride=2,
-                          padding=1, output_padding=0, gen_act=[LF, STanh], nin_channel=1, nout_channel=4, nthresh_layer_gen=3, nthresh_layer_disc=0, dropout_rate = 0.)
+                        nconv_fcdis=64, cuda=cuda, ngpu=4, nconv_layer_gen=4, nconv_layer_disc=5, kernal_size=4,
+                        stride=2,
+                        padding=1, output_padding=0, gen_act=[LF, STanh], nin_channel=1, nout_channel=4,
+                        nthresh_layer_gen=3, nthresh_layer_disc=0, dropout_rate=0.)
 
-#PIXGAN.load_states(model_dir, "_{}".format(13))
 mlflow.set_experiment(PIXGAN.identifier)
 with mlflow.start_run(experiment_id=PIXGAN.experiment.experiment_id) as mlflow_run:
     torch.cuda.empty_cache()
@@ -66,4 +65,3 @@ with mlflow.start_run(experiment_id=PIXGAN.experiment.experiment_id) as mlflow_r
         lambda_gp=10.,
         lambda_l1=100.
     )
-
